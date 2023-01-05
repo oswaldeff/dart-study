0d8f4b5c82982274a400a9b4b4b711b026019514
owaldeff <tialem94@gmail.com>
2023-01-05 22:55:03 +0900

    feat: 변수

---
+++ day1-variable.dart
@@ -0,0 +1,11 @@
+void main() {
+    // variable
+    var name =  'oswladeff';
+    print(name);
+
+    name = 'Dart';
+    print(name);
+
+    var name = 'flutter'; // 변수에 할당된 값을 바꾸는건 가능하나, 변수를 재선언하는것은 불가능
+    print(name);
+}
